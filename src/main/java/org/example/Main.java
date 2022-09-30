324be016c0111222fbfee5e9137e32b98d00f0d3
rehanejaz78 <rehanejaz78@gmail.com>
2022-09-30 13:02:37 +0500

    added lists

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("update in code");
+        System.out.println("update i code 2nd time");
+
+        List<String> list=new ArrayList<String>();
+
+        list.add("rehan");
+        list.add("ejaz");
+        System.out.println(list.stream().count());
+
 
     }
 }
